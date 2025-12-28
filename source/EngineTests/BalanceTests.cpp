df32fe5594f2b983b435b3ebb72ddf3766d5769a
Christian Heinemann <heinemann.christian@gmail.com>
2025-12-28 14:06:19 +0100

    Flaky tests fixed

--- source/EngineTests/BalanceTests.cpp
+++ source/EngineTests/BalanceTests.cpp
@@ -107,6 +107,7 @@ TEST_F(BalanceTests, longRunning_smallCreatures_vs_largeCreatures_fewDigestionCa
 {
     _parameters.externalEnergyControlToggle.value = true;
     _parameters.externalEnergy.value = 200000;
+    _parameters.attackerRadius.value[0] = 4.0f;
     _simulationFacade->setSimulationParameters(_parameters);
 
     auto data = Description().add(createSmallCreatureSeed()).add(createLargeCreatureSeed(DigestionCapability::Low));
@@ -136,6 +137,7 @@ TEST_F(BalanceTests, longRunning_smallCreaturesVsLargeCreatures_highDigestionCap
 {
     _parameters.externalEnergyControlToggle.value = true;
     _parameters.externalEnergy.value = 200000;
+    _parameters.attackerRadius.value[0] = 4.0f;
     _simulationFacade->setSimulationParameters(_parameters);
 
     auto data = Description().add(createSmallCreatureSeed()).add(createLargeCreatureSeed(DigestionCapability::High));
